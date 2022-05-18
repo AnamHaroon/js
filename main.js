f0a03b9ccbc5d88425a6ea0a96ef46d10107909b
AnamHaroon <75257810+AnamHaroon@users.noreply.github.com>
2022-05-19 02:42:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var first_name="Shivam";
+console.log(first_name);
+
+var last_name=" Singh";
+console.log(last_name);
+
+var full_name= first_name.concat(last_name);
+console.log(full_name);
+
+
+
+var number_1= 10;
+console.log(number_1);
+var number_2= 20;
+console.log(number_2);
+
+
+
+var add= number_1+number_2;
+console.log(add);
+
+var sub= 
